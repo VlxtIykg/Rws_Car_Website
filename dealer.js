340e3c348feed15c69ad82ec87a3d57ac97ae064
Kami <qewadsyt@gmail.com>
2022-11-10 18:05:16 +0800

    style: :lipstick: A lot of CSS, some javascript to add interaction.
    CSS -- overview. Added animation, added custom colors, properties/variables, used flex for  adjacent placement of cars, staff, client. Removed lengthy client with a text that appears to write itself using css for the typewriter animation and js for the removal of clients, the  adding the text x amount of customers waiting. JS overview -- fixed droppable and draggable, added a counter to remove the clients, added a counter to add a text that will be editted with css, added a counter 0 for first customer that should run after every dropped. HTML overview --  remove salon controlling every div,  salon now only controls cars_place, staff box (cashier and exit in one div), clients_queue. Clients_queue has also been moved from top of div l16 old file, to l83 last div for placement, made most of dom to be flex, removed button testFunc(). Overview -- removed button, added comments with current Line, removed website icon temporarily as it was causing issues, currently works best with 800px. Current plan is continue fixing css to fit 800px and above, after that finish S3.

--- dealer.js
+++ dealer.js
@@ -2,7 +2,16 @@ const brandList = ["Porsche", "Volkswagen", "Audi", "BMW"]
 
 let counter = 0;
 function newClient(){
-	if(counter >= 2) return;
+	if(counter >= 10) {
+		console.log(`Counter: ${counter}`);
+		let amtOfVisibleCustomers = counter-5
+		const customerAmtDiv = document.createElement("div");
+		customerAmtDiv.id = "aoc";
+		const customerAmtText = document.createTextNode(`${amtOfVisibleCustomers} customers waiting..`);
+		customerAmtDiv.appendChild(customerAmtText);
+		document.getElementById("clients_queue").appendChild(customerAmtDiv);
+		return;
+	};
  	let client = Math.floor((Math.random()*10)+1);
 	$("#clients_queue").append(`
 	<div class="client client_${client} choice_${brandList[0]}">
@@ -11,6 +20,10 @@ function newClient(){
 	if(counter === 0) {
 		document.getElementsByClassName("client")[0].classList.add("first_customer")
 		console.log("rawr");
+	}
+	if(counter >= 5) {
+		const client =  document.getElementsByClassName("client")
+		client[counter].classList.add("disappear")
 	}
 	counter++;
 	$(function() {
@@ -34,24 +47,12 @@ $("document").ready(function(e) {
 			console.log(this.parentElement); //parent element is LI, this tag is the img
 			$(this.parentElement).append(ui.draggable);
 			const firstCustomer = document.getElementsByClassName("first_customer")[0];
-			// console.log();			
 			$(this.parentElement).append(firstCustomer.children[0]);
-			
 			ui.draggable.position({of: $(this), my: 'left top', at: 'left top'});
-			// counter--;
-			// newClient();
+			counter--;
+			newClient();
 		}
 	});
 
 	
 });
-
-const testFunc = () => {
-	const elem = document.getElementsByClassName("choice_Porsche")
-	if ($(elem[0]).data('draggable')) {
-		alert("yes");
-	}
-	else {
-		alert("no");
-	}
-}
