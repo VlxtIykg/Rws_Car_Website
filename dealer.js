973985db23e26a461c548e0f4e40dcd8f42e16ac
Kami <qewadsyt@gmail.com>
2022-11-14 04:27:28 +0800

    style: :lipstick: Adding colors and experimenting
    Going to be adding a javascript thing to check if there are more than 5 customers then add a class that will make it span to two rows too avoid overfilling at the bottom. Going to add a theme checker. Experimenting with themes

--- dealer.js
+++ dealer.js
@@ -1,47 +1,65 @@
 const brandList = ["Porsche", "Volkswagen", "Audi", "BMW"]
 
 let counter = 0;
-function newClient(){
+function NewClient(){
+
+	let elem = document.getElementById("aoc");
+	if(typeof elem !== 'undefined' && elem !== null) {
+		elem.parentNode.removeChild(elem);
+		console.log("Customer amount message deleted.");
+	}
+
 	if(counter >= 10) {
 		console.log(`Counter: ${counter}`);
-		let amtOfVisibleCustomers = counter-5
+		const amountOfClient = document.querySelectorAll("#clients_queue .disappear").length
+		const currentCount = counter-amountOfClient
 		const customerAmtDiv = document.createElement("div");
 		customerAmtDiv.id = "aoc";
-		const customerAmtText = document.createTextNode(`${amtOfVisibleCustomers} customers waiting..`);
+		const customerAmtText = document.createTextNode(`${currentCount} customers waiting..`);
 		customerAmtDiv.appendChild(customerAmtText);
-		document.getElementById("clients_queue").appendChild(customerAmtDiv);
-		return;
+		document.getElementById("clients_queue").prepend(customerAmtDiv);
+		console.log("Customer amount message added.");
+		return false;
 	};
- 	let client = Math.floor((Math.random()*10)+1);
+ 	const client = Math.floor((Math.random()*10)+1);
 	$("#clients_queue").append(`
-	<div class="client client_${client} choice_${brandList[0]}">
-	<span class="preference">Client for ${brandList[0]}</span></div>
+	<div class="clientcard choice_${brandList[0]}">
+	<div class="clientcard__image client_${client} ">
+	</div>
+	<span class="preference">Client for ${brandList[0]}</span>
+	</div>
 	`)
 	if(counter === 0) {
-		document.getElementsByClassName("client")[0].classList.add("first_customer")
-		console.log("rawr");
+		document.getElementsByClassName("clientcard")[0].classList.add("first_customer")
 	}
 	if(counter >= 5) {
-		const client =  document.getElementsByClassName("client")
-		client[counter].classList.add("disappear")
+		const randomNumber = Math.round(Math.random());
+		if(!randomNumber) {
+			const client =  document.getElementsByClassName("clientcard");
+			client[counter].classList.add("disappear");
+		}
+		const amountOfClient = document.querySelectorAll("#clients_queue .disappear").length
+		if(amountOfClient > 5) {
+
+		}
 	}
 	counter++;
 	$(function() {
-		$(".client").draggable();
+		$(".clientcard").draggable();
 		
     // Return the draggable (or it's helper) to its original location when dragging stops with the boolean revert option.
-    $(".client").draggable({ revert: true });
+    $(".clientcard").draggable({ revert: false });
 		
-    $(".client").draggable( "option", "zIndex", 10 );
+    $(".clientcard").draggable( "option", "zIndex", 10 );
 		
   });
 
-	setTimeout(function(){newClient();},100);
+	setTimeout(function(){NewClient();},100);
 }
 $("document").ready(function(e) {
-	newClient();
+	NewClient();
 	
-	$('#porsche > ul > li > img.car').droppable({
+	$('#porsche > ul > li ').droppable({
 		accept: '.choice_Porsche',
 		drop: function(e, ui) {
 			console.log(this.parentElement); //parent element is LI, this tag is the img
@@ -50,7 +68,7 @@ $("document").ready(function(e) {
 			$(this.parentElement).append(firstCustomer.children[0]);
 			ui.draggable.position({of: $(this), my: 'left top', at: 'left top'});
 			counter--;
-			newClient();
+			NewClient();
 		}
 	});
 
