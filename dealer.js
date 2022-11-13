51964ece21c2f0418821258b9ab5b7e670469408
Kami <qewadsyt@gmail.com>
2022-11-14 06:52:56 +0800

    style: :lipstick: First theme added: [Tropical light]

--- dealer.js
+++ dealer.js
@@ -1,6 +1,9 @@
 const brandList = ["Porsche", "Volkswagen", "Audi", "BMW"]
 
 let counter = 0;
+// const client = Math.floor((Math.random()*10)+1);
+const client = 10;
+console.log(client);	
 function NewClient(){
 
 	let elem = document.getElementById("aoc");
@@ -8,20 +11,31 @@ function NewClient(){
 		elem.parentNode.removeChild(elem);
 		console.log("Customer amount message deleted.");
 	}
-
-	if(counter >= 10) {
+ 
+	if(counter >= 10 || counter >= client) {
 		console.log(`Counter: ${counter}`);
 		const amountOfClient = document.querySelectorAll("#clients_queue .disappear").length
 		const currentCount = counter-amountOfClient
 		const customerAmtDiv = document.createElement("div");
 		customerAmtDiv.id = "aoc";
 		const customerAmtText = document.createTextNode(`${currentCount} customers waiting..`);
+		// if(client === 6) {
+		// 	customerAmtDiv.class = "6clients";
+		// }
 		customerAmtDiv.appendChild(customerAmtText);
 		document.getElementById("clients_queue").prepend(customerAmtDiv);
 		console.log("Customer amount message added.");
 		return false;
 	};
- 	const client = Math.floor((Math.random()*10)+1);
+
+	if(client > 5) {
+		const elem = document.getElementById("clients_queue");
+		elem.classList.add("amountOver5");
+	}
+
+	if(client === counter) return;
+
+
 	$("#clients_queue").append(`
 	<div class="clientcard choice_${brandList[0]}">
 	<div class="clientcard__image client_${client} ">
@@ -32,17 +46,7 @@ function NewClient(){
 	if(counter === 0) {
 		document.getElementsByClassName("clientcard")[0].classList.add("first_customer")
 	}
-	if(counter >= 5) {
-		const randomNumber = Math.round(Math.random());
-		if(!randomNumber) {
-			const client =  document.getElementsByClassName("clientcard");
-			client[counter].classList.add("disappear");
-		}
-		const amountOfClient = document.querySelectorAll("#clients_queue .disappear").length
-		if(amountOfClient > 5) {
-
-		}
-	}
+	
 	counter++;
 	$(function() {
 		$(".clientcard").draggable();
